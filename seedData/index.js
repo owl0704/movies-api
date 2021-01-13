aedc914bb4ecbf099df17e4c003ed2b6c756bbd2
lizhixiang <3045055246@qq.com>
2021-01-13 02:15:09 +0000

    Topic 10 - Salt and Hash

--- seedData/index.js
+++ seedData/index.js
@@ -13,12 +13,12 @@ const users = [
 
 // deletes all user documents in collection and inserts test data
 export async function loadUsers() {
-  console.log('load user Data');
-    try {
-      await userModel.deleteMany();
-      await userModel.collection.insertMany(users);
-      console.info(`${users.length} users were successfully stored.`);
-    } catch (err) {
-      console.error(`failed to Load user Data: ${err}`);
+    console.log('load user Data');
+      try {
+        await userModel.deleteMany();
+        await users.forEach(user => userModel.create(user));
+        console.info(`${users.length} users were successfully stored.`);
+      } catch (err) {
+        console.error(`failed to Load user Data: ${err}`);
+      }
     }
-  }
